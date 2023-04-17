b1a4c7e5f3dcb2d46bbbec44d4edd8c5384fea12
shantu03 <hulwanshantanu@gmail.com>
2023-04-17 07:50:46 +0530

    on Pop os

--- srand.c
+++ srand.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<time.h>
-#include<Stdlib.h>
+#include<stdlib.h>
 void main()
 {
 srand(time(0)); //time(0) means current time
 int i; 
 for(int z=1;z<50;z++)
 {
-    i=(rand()%15) +1;    //every time loop execute random value show between 0 to 15; 0 is default set and we get (rand()%15) will be betw 0-14, so we add +1 to get random 1-15
+    i=(rand()%24) +1;    //every time loop execute random value show between 0 to 15; 0 is default set and we get (rand()%15) will be betw 0-14, so we add +1 to get random 1-15
     printf("%d\t",i);
 }
 }
