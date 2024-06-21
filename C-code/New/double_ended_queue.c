d5b4171a4e85e0213cf207324b469e650319be9a
Shantanu Hulwan <117788434+shantu03@users.noreply.github.com>
2024-06-21 09:34:48 +0530

    Create double_ended_queue.c

---
+++ C-code/New/double_ended_queue.c
@@ -0,0 +1,73 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#define max 5
+int front=-1,rear=-1,arr[max],item;
+void dequef()
+{
+    if(front==-1)
+    {
+        printf("underflow at front\n");
+        
+    }else{
+        item=arr[front++];
+        if(front>rear)
+        {
+            front=-1;
+            rear=-1;
+        }
+        printf("deque = %d\n",item);
+    }
+}
+void dequer()
+{
+    if(rear==-1)
+    {
+        printf("underflow at rear\n");
+    }else{
+        
+        item=arr[rear--];
+        
+        if(rear<front)
+        {
+            rear=-1;
+            front=-1;
+        }
+        printf("dequer = %d\n",item);
+    }
+}
+
+void enque()
+{
+    if(rear==max-1)
+    {
+        printf("overflow\n");
+    }else{
+        printf("enter ele\n");
+        scanf("%d",&item);
+        arr[++rear]=item;
+        if(front==-1)   front=0;
+    }
+}
+
+int main()
+{
+    int choice;
+    do{
+        printf("1 for enquer 2 for dequer 3 for dequef 4 for exit\t");
+        scanf("%d",&choice);
+        
+        if(choice==1) enque();
+        if(choice==2) dequer();
+        if(choice==3) dequef();
+        
+    }while(choice!=4);
+
+    return 0;
+}
